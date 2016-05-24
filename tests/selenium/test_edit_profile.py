76cf4398750d60af6421c40b982c4b9908130377
Boris Alex <bo-a@yandex.ru>
2016-05-24 22:25:34 +0300

    Finished negative tests for "edit profile", code clean-ups. Changes to be committed: 	modified:   tests/robot/Test_EditProfile.txt         modified:   tests/selenium/test_edit_profile.py

--- tests/selenium/test_edit_profile.py
+++ tests/selenium/test_edit_profile.py
@@ -17,6 +17,16 @@ class TestPreparations(unittest.TestCase):
     _original_name = "Семищенко Христофор Онуфрійович"
     _original_email = "semuschenko@gmail.com"
 
+    _xpaths = {"frame_title": "//h3[contains(.,'Редагувати профіль')]",
+               "edit_button": "//a[@href='/core/profile_edit/']",
+               "input_name": "//input[@id='inputName']",
+               "input_email": "//input[@id='inputEmail']",
+               "confirm_btn": "//button[@name='confirm_button']",
+               "warnings": "//span[@class='help-block']"}
+
+    _warnings = {"name": "Некоректно введено ім'я.",
+                 "email": "Некоректно введено email."}
+
     def setUp(self):
         """ Fixture that creates a initial data and records for tests,
         initial test steps
@@ -96,13 +106,18 @@ class PositiveTests(TestPreparations):
     expected result - data in profile is changed
     """
 
+    _xpaths = TestPreparations._xpaths
+
+    _warnings = TestPreparations._warnings
+
     _test_data = [
         ["Іванов Петро Степанович", "email@domain.com"],
         ["Стець Мар'яна Іванівна", "marjana.stetc@gmail.com"],
         ["Ворон-Розумна Анна-Марія Степанівна", "email@domain.com"],
         ["Ворон-Розумна Анна Степанівна", "email@domain.com"],
         ["Ворон Анна-Марія Степанівна", "email@domain.com"],
-        ["Ворон Анна Степанівна", "voron-rozumna.anna-marija@many-many.domain.levels.gmail.com"],
+        ["Ворон Анна Степанівна",
+         "voron-rozumna.anna-marija@many-many.domain.levels.gmail.com"],
     ]
 
     def setUp(self):
@@ -122,26 +137,32 @@ class PositiveTests(TestPreparations):
         self.passed = False
 
         driver = self.driver
-        driver.find_element_by_xpath("//a[@href='/core/profile_edit/']").click()
+        driver.find_element_by_xpath(self._xpaths["edit_button"]).click()
         driver.implicitly_wait(10)
-        driver.find_element_by_xpath("//h3[contains(.,'Редагувати профіль')]")
+        driver.find_element_by_xpath(self._xpaths["frame_title"])
+        # waiter = WebDriverWait(driver, 30)
+        # waiter.until(lambda x: x.find_element_by_xpath(self._xpaths["frame_title"]))
 
-        input_name = driver.find_element_by_xpath("//input[@id='inputName']")
+        input_name = driver.find_element_by_xpath(self._xpaths["input_name"])
         input_name.clear()
         input_name.send_keys(self.name.decode('utf-8'))
         input_name.send_keys(Keys.TAB)
 
-        input_email = driver.find_element_by_xpath("//input[@id='inputEmail']")
+        input_email = driver.find_element_by_xpath(self._xpaths["input_email"])
         input_email.clear()
         input_email.send_keys(self.email.decode('utf-8'))
         input_email.send_keys(Keys.TAB)
-        driver.find_element_by_xpath("//button[@name='confirm_button']").click()
+        driver.find_element_by_xpath(self._xpaths["confirm_btn"]).click()
 
-        warnings = driver.find_elements_by_xpath("//span[@class='help-block']")
+        warnings = driver.find_elements_by_xpath(self._xpaths["warnings"])
         for warning in warnings:
             msg = "Positive test failed - " + self.name + " " + self.email
-            self.assertNotEqual("Некоректно введено ім'я.".decode('utf-8'), warning.text, msg)
-            self.assertNotEqual("Некоректно введено email.".decode('utf-8'), warning.text, msg)
+            self.assertNotEqual(self._warnings["name"].decode('utf-8'),
+                                warning.text,
+                                msg)
+            self.assertNotEqual(self._warnings["email"].decode('utf-8'),
+                                warning.text,
+                                msg)
 
         self.assertIn(self.name.decode('utf-8'),
                       driver.find_element_by_xpath(
@@ -212,7 +233,15 @@ class NegativeTests(TestPreparations):
     expected result - login is successful
     """
 
+    _xpaths = TestPreparations._xpaths
+
+    _warnings = TestPreparations._warnings
+
+    _original_name = TestPreparations._original_name
+    _original_email = TestPreparations._original_email
+
     _test_data_names = [
+        "",
         "Мадонна",
         "Мурзік Васильович",
         "Василь Перебійніс",
@@ -226,14 +255,22 @@ class NegativeTests(TestPreparations):
         "Семищенко_Христофор_Онуфрійович",
         "Семищенко Христофор Онуфр1йович",
         "Семищенко Христофор О.",
-        "Семищенкомищенко Христофористофор Онуфрійовичуфрійовичуфрійович"]
+        "Семищенкомищенко Христофористофор Онуфрійовичуфрійовичуфрійович",
+        _original_name + "     ",
+        "     " + _original_name,
+        "     "]
 
     _test_data_emails = [
+        "",
         "@gmail.com",
         "address@.com",
         "address@gmail",
         "address@gmail@com",
-        "voron-rozumna.anna-marija@many-many.domain.levels.and.some.more.domain.levels.for.email.testing.gmail.com"]
+        "voron-rozumna.anna-marija@many-many.domain.levels.and."
+        + "some.more.domain.levels.for.email.testing.gmail.com",
+        _original_email+"     ",
+        "     " + _original_email,
+        "     "]
 
     def setUp(self):
         """ Fixture that creates a initial data and records for tests """
@@ -258,28 +295,34 @@ class NegativeTests(TestPreparations):
         self.passed = False
 
         driver = self.driver
-        driver.find_element_by_xpath("//a[@href='/core/profile_edit/']").click()
+        driver.find_element_by_xpath(self._xpaths["edit_button"]).click()
         driver.implicitly_wait(10)
-        driver.find_element_by_xpath("//h3[contains(.,'Редагувати профіль')]")
+        driver.find_element_by_xpath(self._xpaths["frame_title"])
 
-        input_name = driver.find_element_by_xpath("//input[@id='inputName']")
+        input_name = driver.find_element_by_xpath(self._xpaths["input_name"])
         input_name.clear()
         input_name.send_keys(self.name.decode('utf-8'))
         input_name.send_keys(Keys.TAB)
 
-        input_email = driver.find_element_by_xpath("//input[@id='inputEmail']")
+        input_email = driver.find_element_by_xpath(self._xpaths["input_email"])
         input_email.clear()
         input_email.send_keys(self.email.decode('utf-8'))
         input_email.send_keys(Keys.TAB)
-        driver.find_element_by_xpath("//button[@name='confirm_button']").click()
+        driver.find_element_by_xpath(self._xpaths["confirm_btn"]).click()
 
-        msg = "Negative test failed - " + self.name + " " + self.email
-        warnings = driver.find_elements_by_xpath("//span[@class='help-block']")
-        expected = "Некоректно введено ім'я."
-        self.assertIsNot(any(expected.decode('utf-8') == warning.text for warning in warnings), msg)
-        # self.assertNotEqual("Некоректно введено ім'я.".decode('utf-8'), warning.text, msg)
-        # self.assertNotEqual("Некоректно введено email.".decode('utf-8'), warning.text, msg)
+        msg1 = "Page changed, profile editing passed"
+        msg2 = "Negative test failed - " + self.name + " " + self.email
 
+        self.assertNotEqual(
+            len(driver.find_elements_by_xpath(self._xpaths["frame_title"])),
+            0, msg1 + msg2)
+
+        warnings = driver.find_elements_by_xpath(self._xpaths["confirm_btn"])
+        expected = self._warnings["name"]
+        self.assertIsNot(
+            any(expected.decode('utf-8') == warning.text
+                for warning in warnings),
+            msg2)
 
         self.passed = True
 
@@ -289,34 +332,265 @@ class NegativeTests(TestPreparations):
         self.passed = False
 
         driver = self.driver
-        driver.find_element_by_xpath("//a[@href='/core/profile_edit/']").click()
+        driver.find_element_by_xpath(self._xpaths["edit_button"]).click()
         driver.implicitly_wait(10)
-        driver.find_element_by_xpath("//h3[contains(.,'Редагувати профіль')]")
+        driver.find_element_by_xpath(self._xpaths["frame_title"])
 
-        input_name = driver.find_element_by_xpath("//input[@id='inputName']")
+        input_name = driver.find_element_by_xpath(self._xpaths["input_name"])
         input_name.clear()
         input_name.send_keys(self.name.decode('utf-8'))
         input_name.send_keys(Keys.TAB)
 
-        input_email = driver.find_element_by_xpath("//input[@id='inputEmail']")
+        input_email = driver.find_element_by_xpath(self._xpaths["input_email"])
         input_email.clear()
         input_email.send_keys(self.email.decode('utf-8'))
         input_email.send_keys(Keys.TAB)
-        driver.find_element_by_xpath("//button[@name='confirm_button']").click()
+        driver.find_element_by_xpath(self._xpaths["confirm_btn"]).click()
 
-        msg = "Negative test failed - " + self.name + " " + self.email
-        warnings = driver.find_elements_by_xpath("//span[@class='help-block']")
-        expected = "Некоректно введено email."
-        self.assertIsNot(any(expected.decode('utf-8') == warning.text for warning in warnings), msg)
+        msg1 = "Page changed, profile editing passed \n"
+        msg2 = "Negative test failed - " + self.name + " " + self.email
+
+        self.assertNotEqual(
+            len(driver.find_elements_by_xpath(self._xpaths["frame_title"])),
+            0, msg1 + msg2)
+
+        warnings = driver.find_elements_by_xpath(self._xpaths["confirm_btn"])
+        expected = self._warnings["email"]
+        self.assertIsNot(
+            any(expected.decode('utf-8') == warning.text
+                for warning in warnings),
+            msg2)
 
         self.passed = True
 
-    def test_05(self):
-        """ 05. Edit profile with correct data (first case) """
+    def _test_steps_restore(self):
+        """ Common tests steps """
+
+        self.passed = False
+
+        driver = self.driver
+        driver.find_element_by_xpath(self._xpaths["edit_button"]).click()
+        driver.implicitly_wait(10)
+        driver.find_element_by_xpath(self._xpaths["frame_title"])
+
+        input_name = driver.find_element_by_xpath(self._xpaths["input_name"])
+        input_name.clear()
+        input_name.send_keys(self.name.decode('utf-8'))
+        input_name.send_keys(Keys.TAB)
+
+        input_email = driver.find_element_by_xpath(self._xpaths["input_email"])
+        input_email.clear()
+        input_email.send_keys(self.email.decode('utf-8'))
+        input_email.send_keys(Keys.TAB)
+        driver.find_element_by_xpath(self._xpaths["confirm_btn"]).click()
+
+        warnings = driver.find_elements_by_xpath(self._xpaths["confirm_btn"])
+        for warning in warnings:
+            msg = "Positive test failed - " + self.name + " " + self.email
+            self.assertNotEqual(self._warnings["name"].decode('utf-8'),
+                                warning.text,
+                                msg)
+            self.assertNotEqual(self._warnings["email"].decode('utf-8'),
+                                warning.text,
+                                msg)
+
+        self.assertIn(self.name.decode('utf-8'),
+                      driver.find_element_by_xpath(
+                          "//h3[@class='only-bottom-margin']").text)
+
+        self.assertIn(self.email.decode('utf-8'),
+                      driver.find_element_by_xpath(
+                          "//div[@class='col-md-8']").text)
+        self.passed = True
+
+    def test_11_empty_name(self):
+        """ 11. Edit profile with empty name """
 
         self.current_test = 0
         self._set_test_data_name()
         self._test_steps_name()
+
+    def test_12_incorrect_name(self):
+        """ 12. Edit profile with incorrect name (first case) """
+
+        self.current_test = 1
+        self._set_test_data_name()
+        self._test_steps_name()
+
+    def test_13_incorrect_name(self):
+        """ 13. Edit profile with incorrect name (second case) """
+
+        self.current_test = 2
+        self._set_test_data_name()
+        self._test_steps_name()
+
+    def test_14_incorrect_name(self):
+        """ 14. Edit profile with incorrect name (third case) """
+
+        self.current_test = 3
+        self._set_test_data_name()
+        self._test_steps_name()
+
+    def test_15_incorrect_name(self):
+        """ 15. Edit profile with incorrect name (fourth case) """
+
+        self.current_test = 4
+        self._set_test_data_name()
+        self._test_steps_name()
+
+    def test_16_incorrect_name(self):
+        """ 16. Edit profile with incorrect name (fifth case) """
+
+        self.current_test = 5
+        self._set_test_data_name()
+        self._test_steps_name()
+
+    def test_17_incorrect_name(self):
+        """ 17. Edit profile with incorrect name (sixth case) """
+
+        self.current_test = 6
+        self._set_test_data_name()
+        self._test_steps_name()
+
+    def test_18_incorrect_name(self):
+        """ 18. Edit profile with incorrect name (seventh case) """
+
+        self.current_test = 7
+        self._set_test_data_name()
+        self._test_steps_name()
+
+    def test_19_incorrect_name(self):
+        """ 19. Edit profile with incorrect name (eighth case) """
+
+        self.current_test = 8
+        self._set_test_data_name()
+        self._test_steps_name()
+
+    def test_20_incorrect_name(self):
+        """ 20. Edit profile with incorrect name (ninth case) """
+
+        self.current_test = 9
+        self._set_test_data_name()
+        self._test_steps_name()
+
+    def test_21_incorrect_name(self):
+        """ 21. Edit profile with incorrect name (tenth case) """
+
+        self.current_test = 10
+        self._set_test_data_name()
+        self._test_steps_name()
+
+    def test_22_incorrect_name(self):
+        """ 22. Edit profile with incorrect name (eleventh case) """
+
+        self.current_test = 11
+        self._set_test_data_name()
+        self._test_steps_name()
+
+    def test_23_incorrect_name(self):
+        """ 23. Edit profile with incorrect name (twelfth case) """
+
+        self.current_test = 12
+        self._set_test_data_name()
+        self._test_steps_name()
+
+    def test_24_incorrect_name(self):
+        """ 24. Edit profile with incorrect name (thirteenth case) """
+
+        self.current_test = 13
+        self._set_test_data_name()
+        self._test_steps_name()
+
+    def test_25_incorrect_name(self):
+        """ 25. Edit profile with incorrect name (fourteenth case) """
+
+        self.current_test = 14
+        self._set_test_data_name()
+        self._test_steps_name()
+
+    def test_26_incorrect_name(self):
+        """ 26. Edit profile with correct name that begins with spaces """
+
+        self.current_test = 15
+        self._set_test_data_name()
+        self._test_steps_name()
+
+    def test_27_incorrect_name(self):
+        """ 27. Edit profile with correct name that ends with spaces """
+
+        self.current_test = 16
+        self._set_test_data_name()
+        self._test_steps_name()
+
+    def test_28_incorrect_name(self):
+        """ 28. Edit profile with correct name that consists from spaces """
+
+        self.current_test = 17
+        self._set_test_data_name()
+        self._test_steps_name()
+
+    def test_29_empty_email(self):
+        """ 26. Edit profile with empty email """
+
+        self.current_test = 0
+        self._set_test_data_email()
+        self._test_steps_email()
+
+    def test_30_incorrect_email(self):
+        """ 30. Edit profile with incorrect email (first case) """
+
+        self.current_test = 1
+        self._set_test_data_email()
+        self._test_steps_email()
+
+    def test_31_incorrect_email(self):
+        """ 31. Edit profile with incorrect email (second case) """
+
+        self.current_test = 2
+        self._set_test_data_email()
+        self._test_steps_email()
+
+    def test_32_incorrect_email(self):
+        """ 32. Edit profile with incorrect email (third case) """
+
+        self.current_test = 3
+        self._set_test_data_email()
+        self._test_steps_email()
+
+    def test_33_incorrect_email(self):
+        """ 33. Edit profile with incorrect email (fourth case) """
+
+        self.current_test = 4
+        self._set_test_data_email()
+        self._test_steps_email()
+
+    def test_34_incorrect_email(self):
+        """ 34. Edit profile with incorrect email (fifth case) """
+
+        self.current_test = 5
+        self._set_test_data_email()
+        self._test_steps_email()
+
+    def test_35_incorrect_email(self):
+        """ 35. Edit profile with correct name that begins with spaces """
+
+        self.current_test = 6
+        self._set_test_data_email()
+        self._test_steps_email()
+
+    def test_36_incorrect_email(self):
+        """ 36. Edit profile with correct name that ends with spaces """
+
+        self.current_test = 7
+        self._set_test_data_email()
+        self._test_steps_email()
+
+    def test_37_incorrect_email(self):
+        """ 37. Edit profile with correct name that consists from spaces """
+
+        self.current_test = 8
+        self._set_test_data_email()
+        self._test_steps_email()
 
     def tearDown(self):
         """ Fixture that deletes all preparation for tests and restores
@@ -326,8 +600,7 @@ class NegativeTests(TestPreparations):
         if not self.passed:
             self.name = self._original_name
             self.email = self._original_email
-            self._test_steps_name()
-            self._test_steps_email()
+            self._test_steps_restore()
 
         super(NegativeTests, self).tearDown()
 
