bd6cf051a3f42acbd8b5b17f8efa9c00b375aa9f
Sergio Petrushkevych <psv.programmer@gmail.com>
2016-05-30 07:07:35 +0300

    old files cleanup

--- tests/run_test_search_main_teacher.sh
+++
@@ -1,9 +0,0 @@
-ssh 572956f60c1e66323d0001c6@smscloud-psvatqc.rhcloud.com <<EOF
-mysql < app-root/repo/create_test_db.sql
-cp app-root/repo/wsgi/configurations/conf_test app-root/repo/wsgi/configurations/CONF.ini
-EOF
-pybot test_search_main_teacher.txt
-ssh 572956f60c1e66323d0001c6@smscloud-psvatqc.rhcloud.com <<EOF
-mysql < app-root/repo/drop_test_db.sql
-cp app-root/repo/wsgi/configurations/conf_origin app-root/repo/wsgi/configurations/CONF.ini
-EOF
